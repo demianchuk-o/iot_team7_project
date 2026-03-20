c517f2da2143f5642e65e71bab05e5a335b5e59a
demianchuk-o <demyanchukua@gmail.com>
2026-03-20 11:34:07 +0200

    feat: move to code-first DB approach; removed the sql init scripts

--- store/database.py
+++ store/database.py
@@ -10,4 +10,8 @@ from config import (
 
 DATABASE_URL = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 engine = create_engine(DATABASE_URL)
+
+from models import metadata
+metadata.create_all(bind=engine)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
